05172f6c6e2e6a6f0264d702aa23b5acfea6399e
Anton Burkin <ramzzzay@gmail.com>
2015-10-29 20:52:46 +0300

    Refactoring: enable NinjectControllerFactory

--- CoolShop/Global.asax.cs
+++ CoolShop/Global.asax.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using CoolShop.Infrastructure;
 
 namespace CoolShop
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +14,15 @@ namespace CoolShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_PreSendRequestHeaders()
+        {
+            Response.Headers.Remove("Server");
         }
     }
 }
---
+++ CoolShop/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace CoolShop.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            // создание контейнера 
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext,
+            Type controllerType)
+        {
+            // получение объекта контроллера из контейнера
+            // используя его тип
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            // конфигурирование контейнера 
+
+        }
+    }
+}
