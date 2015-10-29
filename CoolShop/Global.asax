a1ee50c3959bbac5751d5de438b975cda8d0fcd3
Anton Burkin <ramzzzay@gmail.com>
2015-10-29 20:21:18 +0300

    Initial commit

---
+++ CoolShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoolShop.MvcApplication" Language="C#" %>
---
+++ CoolShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoolShop.Startup))]
+namespace CoolShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
