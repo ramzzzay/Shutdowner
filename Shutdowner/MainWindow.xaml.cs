2441bc8532a45aba3557f8918a80088c56be4888
Anton Burkin <ramzzzay@gmail.com>
2016-07-01 12:03:45 +0300

    Change: window mode to do not resize

--- Shutdowner/MainWindow.xaml.cs
+++ Shutdowner/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ namespace Shutdowner
         public MainWindow()
         {
             InitializeComponent();
-            TimeInput.GotFocus += (sender, args) => TimeInput.Text = "" ;
             OkBtn.Click += (sender, args) =>
             {
                 var timer = TimeSpan.FromHours(Convert.ToInt32(TimeInput.Text));
