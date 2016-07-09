6e4f15b0e7657ae8a68e2fb48e8ee4cbbe264c26
Anton Burkin <ramzzzay@gmail.com>
2016-07-09 11:12:53 +0300

    Add: view model

---
+++ CoolShop/Models/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoolShop.Models
+{
+    public class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BorneDate { get; set; }
+    }
+}
